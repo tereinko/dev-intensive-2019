a3c3182846e4463dfa654e947ed5945f155e2c5a
terein <terein@mail.ru>
2019-07-03 20:03:35 +0500

    hometask_2 toInitials()

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -1,5 +1,7 @@
 package ru.skillbranch.devintensive.utils
 
+import java.lang.Character.toUpperCase
+
 object Utils {
     fun parseFullName(fullName:String?):Pair<String?, String?> {
         val parts : List<String>? = fullName?.trim()?.split(" ")
@@ -22,7 +24,16 @@ object Utils {
     }
 
     fun toInitials(firstName: String?, lastName: String?): String? {
-        TODO("may be later")
+        var initials: String? = "";
+
+        if (firstName?.trim().isNullOrEmpty()) {
+            return null // lastName can't be set when ever firstName is not declared
+        }
+        initials = firstName?.trim()?.toUpperCase()?.substring(0, 1)
+        if (!lastName?.trim().isNullOrEmpty()) {
+            initials += lastName?.trim()?.toUpperCase()?.substring(0, 1)
+        }
+        return initials
     }
 
 }
