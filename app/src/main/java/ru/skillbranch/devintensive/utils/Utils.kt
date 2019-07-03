97d500aa500f779f87546a4cb2b67a7d16ac4766
terein <terein@mail.ru>
2019-07-03 19:23:51 +0500

    hometask_2 mandates

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -3,12 +3,18 @@ package ru.skillbranch.devintensive.utils
 object Utils {
     fun parseFullName(fullName:String?):Pair<String?, String?> {
         val parts : List<String>? = fullName?.trim()?.split(" ")
+        val firstName: String? = parts?.getOrNull(0)
+        val lastName: String? = parts?.getOrNull(1)
 
-        val firstName_ = parts?.getOrNull(0)
-
-        val firstName = if (firstName_.isNullOrEmpty()) "null" else parts?.getOrNull(0)
-        val lastName = parts?.getOrNull(1) ?: "null"
-
+        if (firstName.isNullOrEmpty()) {
+            return Pair(null, null) // lastName can't be set when ever firstName is not declared
+        }
+        else {
+            if (lastName.isNullOrEmpty())
+                return Pair(firstName, null)
+            else
+                return Pair(firstName, lastName)
+        }
         return Pair(firstName, lastName)
     }
     fun transliteration(payload: String, divider: String = " " ): String? {
