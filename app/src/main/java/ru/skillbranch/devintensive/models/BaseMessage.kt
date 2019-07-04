81cf0898163ba7c4d51d175b564aaafac7011997
terein <terein@mail.ru>
2019-07-04 23:00:02 +0500

    hometask_2 some more

--- app/src/main/java/ru/skillbranch/devintensive/models/BaseMessage.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/BaseMessage.kt
@@ -13,12 +13,12 @@ abstract class BaseMessage (
     abstract fun formatMessage() : String
     companion object AbstractFactory {
         var lastId = -1
-        fun makeMessage(from: User?, chat: Chat, date: Date = Date(),  payload: Any?,
-                        type: String = "text", isIncoming: Boolean = false): BaseMessage {
+        fun makeMessage(from: User?, chat: Chat, date: Date, type: String = "text", payload: Any?,
+                        isIncoming: Boolean = false): BaseMessage {
             lastId++
             return when (type) {
-                "image" -> ImageMessage("$lastId",from, chat, date = date, image = payload as String)
-                else -> TextMessage("$lastId",from, chat, date = date, text = payload as String)
+                "text" -> TextMessage("$lastId",from, chat, date = date, text = payload as String)
+                else -> ImageMessage("$lastId",from, chat, date = date, image = payload as String)
             }
         }
     }
