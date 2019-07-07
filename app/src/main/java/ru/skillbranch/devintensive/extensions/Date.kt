f958fc1708b4d1df9419cd81f046c3d7b5631858
terein <terein@mail.ru>
2019-07-07 21:35:39 +0500

    hometask_2 humanizeDiff()

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -1,28 +1,84 @@
 package ru.skillbranch.devintensive.extensions
 
-import java.lang.IllegalStateException
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.math.abs
 
 const val SECOND = 1000L
 const val MINUTE = 60 * SECOND
 const val HOUR = 60 * MINUTE
 const val DAY = 24 * HOUR
 
-fun Date.format(pattern:String = "HH:mm:ss dd.MM.yy") : String {
+fun Date.format(pattern: String = "HH:mm:ss dd.MM.yy"): String {
     val dateFormat = SimpleDateFormat(pattern, Locale("ru"))
     return dateFormat.format(this)
 }
 
+fun TimeUnits.plural(value: Int): String {
+    return  ""
+}
+
 fun Date.humanizeDiff(date: Date = Date()): String {
-    return "только что"
+
+    val isFutureDetected: Boolean = this > date
+    val diffSeconds: Long = abs(date.time - this.time) / 1000
+    val diffMinutes: Long = diffSeconds / 60
+    val diffHours: Long = diffSeconds / 3600
+    val diffDays: Long = diffSeconds / 86400
+
+    //println ("date = ${date.time}, this = ${this.time}")
+
+    fun processTenses(sourceString: String): String {
+        if (isFutureDetected)
+            return "через $sourceString"
+        else
+            return "$sourceString назад"
+    }
+
+    fun getEnding(value: Long, form1: String, form2: String, form5: String): String {
+        val ending10 = value % 10
+        val ending100 = value % 100
+
+        if (ending100 > 10 && ending100 < 20)
+            return form5
+        else if (ending10 > 1 && ending10 < 5)
+            return form2
+        else if (ending10 == 1L)
+            return form1
+        else
+            return  form5
+    }
+
+    if (diffSeconds <= 1)
+        return "только что"
+    else if (diffSeconds <= 45)
+        return processTenses("несколько секунд")
+    else if (diffSeconds <= 75)
+        return processTenses("минуту")
+    else if (diffMinutes <= 45)
+        return processTenses(
+            "$diffMinutes ${getEnding(diffMinutes, "минуту", "минуты", "минут")}")
+    else if (diffMinutes <= 75)
+        return processTenses("час")
+    else if (diffHours <= 22)
+        return processTenses(
+            "$diffHours ${getEnding(diffHours, "час", "часа", "часов")}")
+    else if (diffHours <= 26)
+        return processTenses("день")
+    else if (diffDays <= 360)
+        return processTenses(
+            "$diffDays ${getEnding(diffDays, "день", "дня", "дней")}")
+    else if (isFutureDetected)
+        return "более чем через год"
+    else
+        return "более года назад"
+
 }
 
 
+enum class TimeUnits { SECOND, MINUTE, HOUR, DAY }
 
-enum class TimeUnits {SECOND, MINUTE, HOUR, DAY}
-
-fun Date.add(value: Int, units: TimeUnits = TimeUnits.SECOND) : Date {
+fun Date.add(value: Int, units: TimeUnits = TimeUnits.SECOND): Date {
     var time = this.time
 
     time += when (units) {
